198702747c4fde5dd39b933471eccc3db4a8930a
Alejandro Sajaroff <asajaroff@users.noreply.github.com>
2020-04-12 13:37:20 -0300

    Clase 3 (#1)
    * Se agregan los Ejercicios
    
    * Primera version
    
    * Agrego el folder ahora gitignore funciona bien
    
    * Primera version funcionando, pendiente agregar acentos
    
    * Funcionando completo pero con muchos if
    
    * mal el output
    
    * Ejercicio 3_1 funcionando

---
+++ Clases/Ejercicios/3_2/app/Program.cs
@@ -0,0 +1,38 @@
+﻿// El usuario ingresará frases hasta que ingrese **FIN**.
+// Mostrar en pantalla la frase que tiene más vocales.
+// Considerar mayúsculas, minúsculas y acentos.
+
+using System;
+
+namespace StringsCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vocalesCount=0, vocalesMax=0;
+            string line, largest="none"; 
+            Console.WriteLine("Ingresar frase: \n");
+            while ((line = Console.ReadLine()) != "FIN") 
+            {
+                vocalesCount = 0;
+                foreach (char c in line)
+                {
+                    char lowercased = char.ToLower(c);
+                    if (lowercased == 'a' || lowercased == 'e'|| lowercased == 'i'|| lowercased == 'o'|| lowercased == 'u' ||
+                        lowercased == 'à' || lowercased == 'è' ||  lowercased == 'ì' ||  lowercased == 'ò' || lowercased == 'ù' ||
+                        lowercased == 'á' || lowercased == 'é' ||  lowercased == 'í' ||  lowercased == 'ó' || lowercased == 'ú' )
+                    {
+                        vocalesCount++;
+                    }
+                    if (vocalesCount > vocalesMax)
+                    {
+                        largest = line;
+                        vocalesMax = vocalesCount;
+                    }
+                }
+            }
+        Console.WriteLine($"Frase con mas vocales:\t{largest}\nCantidad de vocales: \t{vocalesMax}");
+        }
+    }
+}
