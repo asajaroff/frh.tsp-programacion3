61e2b9e0e252374a3a0db5eac1b541cf2c8f36d2
Alejandro Sajaroff <asajaroff@gmail.com>
2020-04-11 21:11:51 -0300

    Primera version

---
+++ Clases/Ejercicios/3_2/app/Program.cs
@@ -0,0 +1,31 @@
+﻿// El usuario ingresará frases hasta que ingrese **FIN**.
+// Mostrar en pantalla la frase que tiene más vocales.
+// Considerar mayúsculas, minúsculas y acentos.
+
+using System;
+
+namespace StringsCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] handler;
+            int vocalesCount;
+            string line, largest; 
+            while ((line = Console.ReadLine()) != 'FIN') 
+            {
+                handler = line.ToCharArray();
+                for (int i = 0; i >= line.Length; i++ ) 
+                {
+                    if (handler[i] == 'A')
+                    {
+                        Console.WriteLine("%i", handler[0]);
+                    }
+                Console.WriteLine()
+                }
+            }
+            Console.WriteLine(largest)
+        }
+    }
+}
